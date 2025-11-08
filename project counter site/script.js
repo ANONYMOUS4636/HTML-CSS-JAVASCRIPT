dfb38e46f948601582da1e2368e5ecafd64d2a01
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2025-11-09 05:11:25 +0530

    added counter site

---
+++ project counter site/script.js
@@ -0,0 +1,13 @@
+let count=document.querySelector(".count")
+let btn_add=document.querySelector(".btn-add")
+let btn_reset=document.querySelector(".btn-reset")
+let a=1;
+btn_add.addEventListener("click",(e)=>{
+    count.innerHTML=a
+    a+=1
+})
+btn_reset.addEventListener("click",(e)=>{
+    count.innerHTML="0"
+    a=0
+})
+
