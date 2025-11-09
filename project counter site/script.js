3b6e916cc86ed376c3be03f2862acb39d603e4ef
Dattatraya Behera <beheradattatraya123+github@gmail.com>
2025-11-09 05:36:24 +0530

    done modification in counter site

--- project counter site/script.js
+++ project counter site/script.js
@@ -3,6 +3,7 @@ let btn_add=document.querySelector(".btn-add")
 let btn_reset=document.querySelector(".btn-reset")
 let a=1;
 btn_add.addEventListener("click",(e)=>{
+    console.log(e.target)
     count.innerHTML=a
     a+=1
 })
